ebf31986edd6cc4204d13d68b3afe35b164ac06a
Ejo415 <ejo7220@gmail.com>
2020-08-19 14:38:06 -0400

    updating cli

--- lib/breweries/cli.rb
+++ lib/breweries/cli.rb
@@ -5,10 +5,22 @@ class CLI
         puts "Please enter a zipcode"
         input = gets.strip 
         API.get_breweries(input)
-        Breweries.all.each.with_index(1) do |x, index|
-            puts "#{index} - #{x.name}"
+        display
+        puts "Please select brewery by number."
+       
+       
         end
     end
+
+  def display
+    Breweries.all.each.with_index(1) do |x, index|
+        puts "#{index} - #{x.name}"
+    end
+
+    def display_info
+
+    end
+
 
    
 end
